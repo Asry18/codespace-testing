a2770061472db84407fdf1ac4408b00250d0bbea
Asry <117133214+Asry18@users.noreply.github.com>
2025-01-03 15:10:32 +0000

    changed to my details-Asry

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Mohamed Asry",
+  title: "Web Designer & App Developer",
+  email: "asryamaz000@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
